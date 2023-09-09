8327bdace53a1c23e2e23a7acba6a3637b1fdc34
Shumbabala <notoriousreking@gmail.com>
2023-09-09 21:31:22 +0200

    MealItem needs form

--- FoodOrderApp/src/components/Layout/Header/Header.js
+++ FoodOrderApp/src/components/Layout/Header/Header.js
@@ -5,13 +5,14 @@ import styles from "./Header.module.css";
 
 //other imports
 import image from "../../../assets/meals.jpg";
+import HeaderCartButton from "../HeaderCartButton/HeaderCartButton";
 
 export default function Header(props) {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles["main-image"]}>
         <img src={image} alt="a table topped with lots of food" />
